1f0818184a570ab96571f1d69324d0dcffbc45a2
Ricardo <ricardopugar@gmail.com>
2025-12-17 19:02:44 -0500

    rutaje

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   // Genera HTML estático en la carpeta `out`
   output: "export",
 
+  // Hace que /ciberseguridad/fortinet sea una carpeta con index.html
+  // en lugar de fortinet.html
+  trailingSlash: true,
+
   // Como lo vas a subir a cPanel, desactivamos la optimización de imágenes
   // del lado del servidor de Next.
   images: {
